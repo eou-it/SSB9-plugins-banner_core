ca2a3b637075b44de01e9870e46e3bfbf8d06de0
Bob Rullo <robert.rullo@sungardhe.com>
2010-05-13 13:56:50 -0400

    Updated properties

--- BannerCoreGrailsPlugin.groovy
+++ BannerCoreGrailsPlugin.groovy
@@ -49,7 +49,7 @@ class BannerCoreGrailsPlugin {
     // independent of deploying a new plugin build to Nexus. 
     String version = "0.1-SNAPSHOT"
 
-    //String version = "0.1.4" 
+    //String version = "0.1.5" 
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.0 > *"
