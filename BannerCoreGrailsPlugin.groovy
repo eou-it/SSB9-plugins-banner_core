822ba9474d48f686076c70feccfd46b8ed1f468e
charlie hardt <chasrails@me.com>
2010-05-15 00:20:24 -0400

    Updated version to 0.1.6, app exections try prepending classname then 'default' when trying to localize, expose appropriate httpStatusCode

--- BannerCoreGrailsPlugin.groovy
+++ BannerCoreGrailsPlugin.groovy
@@ -49,7 +49,7 @@ class BannerCoreGrailsPlugin {
     // independent of deploying a new plugin build to Nexus. 
     //
     //String version = "0.1-SNAPSHOT"
-    String version = "0.1.5" 
+    String version = "0.1.6" 
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.0 > *"
