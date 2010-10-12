ae3aa2f766cc6bbbf953b900179b7e78b41d5cd0
Charlie Hardt <chasdev@me.com>
2010-10-12 13:02:42 -0400

    Updated version number

--- BannerCoreGrailsPlugin.groovy
+++ BannerCoreGrailsPlugin.groovy
@@ -63,7 +63,7 @@ class BannerCoreGrailsPlugin {
     // independent of deploying a new plugin build to Nexus.
     //
     //String version = "0.1-SNAPSHOT"
-    String version = "0.1.20"
+    String version = "0.1.21"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.0 > *"
