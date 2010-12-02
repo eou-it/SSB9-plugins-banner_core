9bc1987db9c6aed40d2d2d6089596dc9898dfe2e
chasdev <chasdev@me.com>
2010-12-02 11:15:15 -0500

    Setting version back to 0.2.3, as SNAPSHOT is not working on CI server

--- BannerCoreGrailsPlugin.groovy
+++ BannerCoreGrailsPlugin.groovy
@@ -62,8 +62,8 @@ class BannerCoreGrailsPlugin {
     // more control on 'when' a grails app is updated to use a newer plugin version, and therefore 'could' allow delayed testing within those apps
     // independent of deploying a new plugin build to Nexus.
     //
-    String version = "0.1-SNAPSHOT"
-//    String version = "0.2.3"
+//    String version = "0.1-SNAPSHOT"
+    String version = "0.2.3"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.0 > *"
