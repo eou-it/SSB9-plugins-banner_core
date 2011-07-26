b23a45aad4813b60afe5e81177a96ca575351275
Rajanand P K <rajanand.pk@sungard.com>
2011-07-26 10:48:38 -0400

    incremented version to 0.3.6

--- BannerCoreGrailsPlugin.groovy
+++ BannerCoreGrailsPlugin.groovy
@@ -76,7 +76,7 @@ class BannerCoreGrailsPlugin {
     // independent of deploying a new plugin build to Nexus.
     //
 //    String version = "0.1-SNAPSHOT"
-    String version = "0.3.5"
+    String version = "0.3.6"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.0 > *"
