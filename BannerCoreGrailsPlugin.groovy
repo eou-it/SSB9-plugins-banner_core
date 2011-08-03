957a9131569a721b97d64a7a81114c4cd51c5e9e
Bob Rullo <robert.rullo@sungardhe.com>
2011-08-03 13:38:11 -0400

    Rev'd to 0.3.8

--- BannerCoreGrailsPlugin.groovy
+++ BannerCoreGrailsPlugin.groovy
@@ -76,7 +76,7 @@ class BannerCoreGrailsPlugin {
     // independent of deploying a new plugin build to Nexus.
     //
 //    String version = "0.1-SNAPSHOT"
-    String version = "0.3.7"
+    String version = "0.3.8"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.0 > *"
