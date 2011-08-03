217a8ddbd1c588b962aea5a18d13d3c4d22a4050
Bob Rullo <robert.rullo@sungardhe.com>
2011-08-03 13:32:57 -0400

    Added support for a /resource access point for rest resources that do not use basic auth, but the already authenticated

--- test/test-banner-core/test/functional/com/sungardhe/banner/testing/FooControllerFunctionalTests.groovy
+++ test/test-banner-core/test/functional/com/sungardhe/banner/testing/FooControllerFunctionalTests.groovy
@@ -106,6 +106,22 @@ class FooControllerFunctionalTests extends BaseFunctionalTestCase {
     }
 
 
+    void testListResource_JSON() {
+        login()
+
+        get( "/resource/foobar?max=$pageSize" ) {
+            headers[ 'Content-Type' ] = 'application/json'
+        }
+        assertStatus 200
+        assertEquals 'application/json', page?.webResponse?.contentType
+
+        def stringContent = page?.webResponse?.contentAsString
+        def data = JSON.parse( stringContent )
+        assertTrue 45 <= data.totalCount
+        assertEquals 5, data.data.size()
+    }
+
+
     void testShow_JSON() {
 
         get( "/api/foobar/1" ) {
