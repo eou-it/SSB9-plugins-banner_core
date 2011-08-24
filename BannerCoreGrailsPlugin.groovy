7de84b331019a20c6cf50084d89b28556717342e
Rajesh Kumar <rajesh.kumar@sungardhe.com>
2011-08-24 12:23:32 -0400

    Updating baner core to 12, adding spring dependency to 1.0.1

--- BannerCoreGrailsPlugin.groovy
+++ BannerCoreGrailsPlugin.groovy
@@ -76,7 +76,7 @@ class BannerCoreGrailsPlugin {
     // independent of deploying a new plugin build to Nexus.
     //
 //    String version = "0.1-SNAPSHOT"
-    String version = "0.3.12"
+    String version = "0.3.13"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.0 > *"
