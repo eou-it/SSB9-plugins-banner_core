eb939b19fc355c22489b2552177244348c4f5ed5
Rajesh Kumar <rajesh.kumar@sungardhe.com>
2011-09-23 09:55:54 -0400

    Updates to Dynamic finder for query support.Update to 1.0.2

--- grails-app/utils/com/sungardhe/banner/query/DynamicFinder.groovy
+++ grails-app/utils/com/sungardhe/banner/query/DynamicFinder.groovy
@@ -1,8 +1,8 @@
 package com.sungardhe.banner.query
 
-import org.springframework.context.ApplicationContext
 import org.codehaus.groovy.grails.web.context.ServletContextHolder
 import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes
+import org.springframework.context.ApplicationContext
 
 /*********************************************************************************
  Copyright 2009-2011 SunGard Higher Education. All Rights Reserved.
@@ -10,85 +10,70 @@ import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes
  SunGard Higher Education and its subsidiaries. Any use of this software is limited 
  solely to SunGard Higher Education licensees, and is further subject to the terms 
  and conditions of one or more written license agreements between SunGard Higher 
- Education and the licensee in question. SunGard is either a registered trademark or
- trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.
- Banner and Luminis are either registered trademarks or trademarks of SunGard Higher 
- Education in the U.S.A. and/or other regions and/or countries.
+ Education and the licensee in question. SunGard, Banner and Luminis are either 
+ registered trademarks or trademarks of SunGard Higher Education in the U.S.A. 
+ and/or other regions and/or countries.
  **********************************************************************************/
 class DynamicFinder {
 
     def domainClass
     def query
     def tableIdentifier
-    def requiredFields
-    def queryableFields
     def criteria
     def pagingAndSortParams
-    def headerMap
 
-    public DynamicFinder( domainClass, query, tableIdentifier, requiredFields, queryableFields, headerMap ) {
+    public DynamicFinder( domainClass, query, tableIdentifier) {
 		this.domainClass = domainClass
 		this.query = query
 		this.tableIdentifier = tableIdentifier
-		this.requiredFields = requiredFields
-		this.queryableFields = queryableFields
-		this.headerMap = headerMap
 	}
 
-    public def find( criteria, pagingAndSortParams ){
+    public def find( filterData, pagingAndSortParams ){
        // This is a convenience method to take a more readable multiline string and collapse it down to a one line string
         String.metaClass.flattenString = {
             return delegate.replace( "\n", "" ).replaceAll( /  */, " " )
         }
 
-        def sortColumnName
-        if(pagingAndSortParams.sortColumn){
-            sortColumnName = headerMap.get(pagingAndSortParams.sortColumn)
-        }
-        pagingAndSortParams.put("sortColumn",sortColumnName)
-        def queryString =  com.sungardhe.banner.query.QueryBuilder.buildQuery( query.flattenString(), tableIdentifier, requiredFields, queryableFields ,criteria,pagingAndSortParams )
+        def queryString =  QueryBuilder.buildQuery( query.flattenString(), tableIdentifier, filterData.criteria ,pagingAndSortParams )
 
-        def list =  domainClass.findAll( queryString, criteria, pagingAndSortParams )
+        def list =  domainClass.findAll( queryString, filterData.params, pagingAndSortParams )
 
         return list
     }
 
-    public def count ( criteria ) {
+    public def count ( filterData ) {
         String.metaClass.flattenString = {
             return delegate.replace( "\n", "" ).replaceAll( /  */, " " )
         }
 
-        def queryString =  com.sungardhe.banner.query.QueryBuilder.buildCountQuery( query.flattenString(), tableIdentifier, requiredFields, queryableFields ,criteria )
-        def returnListCount = domainClass.executeQuery( queryString, criteria )
+        def queryString =  QueryBuilder.buildCountQuery( query.flattenString(), tableIdentifier, filterData.criteria )
+
+        def returnListCount = domainClass.executeQuery( queryString, filterData.params )
 
         return returnListCount[0]
     }
 
-    public static def fetchAll ( domainClass, query, tableIdentifier, requiredFields, queryableFields, criteria, pagingAndSortParams, headerMap) {
+    public static def fetchAll ( domainClass, query, tableIdentifier, filterData, pagingAndSortParams) {
         // This is a convenience method to take a more readable multiline string and collapse it down to a one line string
-              String.metaClass.flattenString = {
+        String.metaClass.flattenString = {
             return delegate.replace( "\n", "" ).replaceAll( /  */, " " )
         }
 
-        def sortColumnName
-        if(pagingAndSortParams.sortColumn){
-            sortColumnName = headerMap.get(pagingAndSortParams.sortColumn)
-        }
-        pagingAndSortParams.put("sortColumn",sortColumnName)
-        def queryString =  com.sungardhe.banner.query.QueryBuilder.buildQuery( query.flattenString(), "a", requiredFields, queryableFields ,criteria,pagingAndSortParams )
+        def queryString =  QueryBuilder.buildQuery( query.flattenString(), "a", filterData.criteria,pagingAndSortParams )
 
-        def list =  domainClass.findAll( queryString, criteria, pagingAndSortParams )
+        def list =  domainClass.findAll( queryString, filterData.params, pagingAndSortParams )
 
         return list
     }
 
-    public static def countAll (domainClass, query, tableIdentifier, requiredFields, queryableFields, criteria) {
-              String.metaClass.flattenString = {
+    public static def countAll (domainClass, query, tableIdentifier, filterData) {
+        String.metaClass.flattenString = {
             return delegate.replace( "\n", "" ).replaceAll( /  */, " " )
         }
 
-        def queryString =  com.sungardhe.banner.query.QueryBuilder.buildCountQuery( query.flattenString(), "a", requiredFields, queryableFields ,criteria )
-        def returnListCount = domainClass.executeQuery( queryString, criteria )
+        def queryString =  QueryBuilder.buildCountQuery( query.flattenString(), "a", filterData.criteria )
+
+        def returnListCount = domainClass.executeQuery( queryString, filterData.params )
 
         return returnListCount[0]
     }
--- grails-app/utils/com/sungardhe/banner/query/QueryBuilder.groovy
+++ grails-app/utils/com/sungardhe/banner/query/QueryBuilder.groovy
@@ -1,103 +1,54 @@
-package com.sungardhe.banner.query
-
 /*********************************************************************************
  Copyright 2009-2011 SunGard Higher Education. All Rights Reserved.
  This copyrighted software contains confidential and proprietary information of 
  SunGard Higher Education and its subsidiaries. Any use of this software is limited 
  solely to SunGard Higher Education licensees, and is further subject to the terms 
  and conditions of one or more written license agreements between SunGard Higher 
- Education and the licensee in question. SunGard is either a registered trademark or
- trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.
- Banner and Luminis are either registered trademarks or trademarks of SunGard Higher 
- Education in the U.S.A. and/or other regions and/or countries.
+ Education and the licensee in question. SunGard, Banner and Luminis are either 
+ registered trademarks or trademarks of SunGard Higher Education in the U.S.A. 
+ and/or other regions and/or countries.
  **********************************************************************************/
+package com.sungardhe.banner.query
+
 class QueryBuilder {
 
-    public static def buildQuery = { query, tableIdentifier, requiredFields, queryableFields, criteria,pagingAndSortParams ->
-        // Ensure that the requird fields are indeed present.
-        requiredFields.each {
-            if (criteria[it] == null) {
-                throw new Exception("'$it' is required and not found in $criteria")
-            }
-        }
-
-        // Validate that the criteria contains only valid codes
-        def validKeys = requiredFields + queryableFields.keySet()
-        criteria.keySet().each {
-            if (!validKeys.contains(it)) {
-                throw new Exception("'$it' is not a valid field key.  Valid keys are $validKeys.")
-            }
-        }
+    public static def buildQuery = { query, tableIdentifier,  criteria,pagingAndSortParams ->
 
         def returnQuery = query
 
-        queryableFields.each {
-            if (criteria[it.key]) {
-                if (it.value instanceof String) {
-                    returnQuery += " and lower($tableIdentifier.${it.value}) like lower(:${it.key})"
-                }
-                else {
-                    returnQuery += " and ("
-                    it.value.eachWithIndex { field, index ->
-                        if (index != 0) {
-                            returnQuery += " or "
-                        }
-
-                        returnQuery += "lower($tableIdentifier.${field.value}) like lower(:${it.key})"
-                    }
-
-                    returnQuery += ")"
-                }
-            }
+        if ( ! (returnQuery as String).toUpperCase().contains (" WHERE ") ) {
+            returnQuery += " WHERE 1=1 "
         }
+
+        criteria.each {
+            returnQuery += CriteriaOperatorFactory.operators."${it.operator}"?.dynamicQuery(tableIdentifier,it)
+        }
+
         if( pagingAndSortParams.sortColumn && (pagingAndSortParams.sortDirection!=null) ){
              returnQuery += "order by  $tableIdentifier.$pagingAndSortParams.sortColumn  $pagingAndSortParams.sortDirection"
+        } else if(pagingAndSortParams.sortColumn){
+              // we are not using tableIdentifier since there is only one table identifier
+              // and no need to add table identifier
+              // and there is not provision for multiple table identifiers as of now.
+              String sort =  (pagingAndSortParams.sortColumn as String).replaceAll("@@table@@", "$tableIdentifier." )
+              returnQuery += "order by $sort "
         }
 
         return returnQuery
     }
 
 
-    public static def buildCountQuery = { query, tableIdentifier, requiredFields, queryableFields, criteria ->
-        // Ensure that the requird fields are indeed present.
-        requiredFields.each {
-            if (criteria[it] == null) {
-                throw new Exception("'$it' is required and not found in $criteria")
-            }
-        }
-
-        // Validate that the criteria contains only valid codes
-        def validKeys = requiredFields + queryableFields.keySet()
-        criteria.keySet().each {
-            if (!validKeys.contains(it)) {
-                throw new Exception("'$it' is not a valid field key.  Valid keys are $validKeys.")
-            }
-        }
+    public static def buildCountQuery = { query, tableIdentifier, criteria ->
 
         def returnQuery = query
 
-        queryableFields.each {
-            if (criteria[it.key]) {
-                if (it.value instanceof String) {
-                    returnQuery += " and lower($tableIdentifier.${it.value}) like lower(:${it.key})"
-                }
-                else {
-                    returnQuery += " and ("
-                    it.value.eachWithIndex { field, index ->
-                        if (index != 0) {
-                            returnQuery += " or "
-                        }
-
-                        returnQuery += "lower($tableIdentifier.${field.value}) like lower(:${it.key})"
-                    }
-
-                    returnQuery += ")"
-                }
-            }
+        if ( ! (returnQuery as String).toUpperCase().contains (" WHERE ") ) {
+            returnQuery += " WHERE 1=1 "
         }
-        /*if( pagingAndSortParams.sortColumn && (pagingAndSortParams.sortDirection!=null) ){
-             returnQuery += "order by  $tableIdentifier.$pagingAndSortParams.sortColumn  $pagingAndSortParams.sortDirection"
-        }*/
+         criteria.each {
+             returnQuery += CriteriaOperatorFactory.operators."${it.operator}"?.dynamicQuery(tableIdentifier,it)
+        }
+
         returnQuery = "select count(*) ${returnQuery}"
         return returnQuery
     }
