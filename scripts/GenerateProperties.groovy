88b18cf8e13346666f83528f0b1ad73af56905c9
Bob Rullo <robert.rullo@sungardhe.com>
2011-10-11 17:35:47 -0400

    Added GenerateProperties script to generate pseudo properties for testing purposes.

--- scripts/GenerateProperties.groovy
+++ scripts/GenerateProperties.groovy
@@ -10,23 +10,199 @@
   Education in the U.S.A. and/or other regions and/or countries.
  ****************************************************************************** */
 
+includeTargets << grailsScript("Init")
+includeTargets << grailsScript("_GrailsInit")
+
+import grails.util.BuildSettingsHolder
+import groovy.io.FileType
+import java.math.MathContext
+import java.math.RoundingMode
+import org.apache.commons.lang.StringUtils
+
 /**
- * Gant script that generates a psuedo properties file for debugging i18n issues.
+ * This script will take a properties bundle and generate a 'dummy' bundle with markup that will
+ * allow us identify a property value as being localized or more importantly, something that is not localized.
  */
 
-includeTargets << new File( "${basedir}/scripts/_GenerateProperties.groovy" )
+def getNewFileName(file, locale) {
+    def absolutePathOfFile = file.absolutePath
+    def newFileName = absolutePathOfFile.replace(".properties", "_" + locale + ".properties")
+    return newFileName
+}
 
-target (generatePropertiesMain:'''Generates a psuedo properties file for debugging i18n issues.
+def writeToFile(fileName, generatedProps) {
+    def destFile = new File( fileName )
+    destFile.delete()
+
+    generatedProps.each { line ->
+        destFile.append(line)
+        destFile.append("\n")
+    }
+}
+
+def shouldFormatKey(key) {
+    def shouldFormat = true;
+
+    if (key.indexOf(".format") >= 0
+            || "default.language.direction".equals(key)) {
+        shouldFormat = false
+    }
+    return shouldFormat
+}
+
+
+def generateNewProp( line, params ) {
+    def isProperty = {
+        return it.contains( "=" )
+    }
+
+    if (isProperty( line ) ) {
+
+        def key = StringUtils.substringBefore( line, "=" )
+        def value = StringUtils.substringAfter( line, "=" )
+
+        if (key == "default.language.locale") {
+            return "default.language.locale=" + params.targetLocale
+        }
+        else if (shouldFormatKey( key )) {
+
+            def calculateBufferSize = { word ->
+                def factor = 0.0
+
+                switch (word.size()) {
+                    case 1..5:
+                        factor = 1
+                        break
+                    case 6..12:
+                        factor = 0.8
+                        break
+                    case 13..20:
+                        factor = 0.6
+                        break
+                    case 21..30:
+                        factor = 0.4
+                        break
+                    case 31..50:
+                        factor = 0.2
+                        break
+                    default:
+                        factor = 0.1
+                }
+
+               return word.size() * factor
+            }
+
+            def numberOfCharactersToAdd = 0.0
+
+            def words = value.tokenize( " " )
+            words.each {
+                numberOfCharactersToAdd += calculateBufferSize( it )
+            }
+
+            def buffer = { num, character ->
+
+                // We will always round up no matter the first precision.
+                def mc = new MathContext( 1, RoundingMode.UP )
+                def rounded = num.round( mc )
+
+                def output = ""
+
+                rounded.times {
+                    output += "@"
+                }
+
+                return output
+            }
+
+            def bufferCharacter = buffer( (numberOfCharactersToAdd / 2), "@" )
+
+            return "$key=$bufferCharacter$value$bufferCharacter"
+        }
+        else {
+            return line
+        }
+    }
+    else {
+        // Return the line as is and do nothing.  We are only generating properties.
+        return line
+    }
+}
+
+/**
+ * This will generate the properties files.  Eventually it'll allow you to pass in an arguments map.
+ * @param args
+ * @return
+ */
+def generatePropertiesFile( args = null ) {
+
+    def params = getParamsMap( args )
+
+
+    def dir = BuildSettingsHolder.settings.baseDir
+    def generatedProps = []
+    dir.traverse(type: FileType.FILES, nameFilter: params.filter) { source ->
+        println "Generating props for " + source.canonicalFile
+        source.eachLine { line ->
+            generatedProps << generateNewProp(line, params)
+        }
+
+        def newFile = getNewFileName(source, params.targetLocale)
+
+        println "Writing to file '$newFile'"
+        def destFile = new File(newFile)
+
+        // Delete the existing file if it is there
+        destFile.delete()
+
+        generatedProps.each { line ->
+            destFile.append(line)
+            destFile.append("\n")
+        }
+    }
+}
+
+def getParamsMap( argMap = [:] ) {
+
+	def paramMap = [:]
+
+    def defaultMap = [
+            dummyCharacter: "@",
+            filter: "messages.properties",
+            targetLocale: "en_PS" // English Pseduo
+    ]
+
+	paramMap.putAll( defaultMap )
+	paramMap.putAll( argMap )
+
+    return paramMap
+}
+
+
+target (main:'''Generates a psuedo properties file for debugging i18n issues.
 
 Examples:
 grails generate-locale-props sourceFile=<source file> targetLocale=<locale>'
-sourceFile: A properties file to dummy translate.  Default is messages.properties * Optional
+filter: A properties file to dummy translate.  Default is messages.properties * Optional
 targetLocale: A dummy locale determining the name of the output file.  Default is 'en_PS' * Optional
 
-E.g.: grails generate-locale-props sourceFile=messages.properties targetLocale=en_PS
+E.g.: grails generate-locale-props filter=messages.properties targetLocale=en_PS
 
 ''') {
-    depends(generateProperties)
+	
+    Number startTime = System.currentTimeMillis()
+    println "Start Generation"
+
+	// Turn args into a map
+	def argMap = [:]
+	args?.split("\n")?.each {
+        def key = StringUtils.substringBefore( it, "=" )
+        def value = StringUtils.substringAfter( it, "=" )
+		argMap[key] = value
+	}
+
+    generatePropertiesFile( argMap )
+
+    println "Generation Complete in " + (System.currentTimeMillis() - startTime) + "ms"
 }
 
-setDefaultTarget('generatePropertiesMain')
+setDefaultTarget('main')
