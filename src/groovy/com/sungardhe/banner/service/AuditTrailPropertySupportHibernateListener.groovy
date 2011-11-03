d27dcc427bc8443a88d0999e4618876382195bdd
Rajesh Kumar <rajesh.kumar@sungardhe.com>
2011-11-03 09:04:55 -0400

    Fix for missing lastmodifiedBy in audit trail proerties

--- src/groovy/com/sungardhe/banner/service/AuditTrailPropertySupportHibernateListener.groovy
+++ src/groovy/com/sungardhe/banner/service/AuditTrailPropertySupportHibernateListener.groovy
@@ -1,12 +1,12 @@
 /*********************************************************************************
  Copyright 2009-2011 SunGard Higher Education. All Rights Reserved.
- This copyrighted software contains confidential and proprietary information of 
- SunGard Higher Education and its subsidiaries. Any use of this software is limited 
- solely to SunGard Higher Education licensees, and is further subject to the terms 
- and conditions of one or more written license agreements between SunGard Higher 
+ This copyrighted software contains confidential and proprietary information of
+ SunGard Higher Education and its subsidiaries. Any use of this software is limited
+ solely to SunGard Higher Education licensees, and is further subject to the terms
+ and conditions of one or more written license agreements between SunGard Higher
  Education and the licensee in question. SunGard is either a registered trademark or
  trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.
- Banner and Luminis are either registered trademarks or trademarks of SunGard Higher 
+ Banner and Luminis are either registered trademarks or trademarks of SunGard Higher
  Education in the U.S.A. and/or other regions and/or countries.
  **********************************************************************************/
 package com.sungardhe.banner.service
@@ -60,25 +60,25 @@ class AuditTrailPropertySupportHibernateListener implements PreInsertEventListen
     }
 
 
-    // Sets the three audit trail fields: 
+    // Sets the three audit trail fields:
     // lastModified - the time with 'second' resolution    (created here)
     // lastModifiedBy - the person who modified the record (taken from the authentication token of the user)
     // dataOrigin - the system that modified the value     (taken from Config.groovy)
     boolean updateSystemFields( event ) {
-        // Unfortunately, 'lastModified' is mapped to SQL DATE type versus SQL TIMESTAMP, and thus does not retain fractional seconds. 
-        // More unfortunately, the Hibernate cache does not reflect this, but will hold the 'lastModified' instance with fractional seconds 
-        // as the 'persistentValue'. Usually this is ok, but if a 'refresh()' is subsequently performed on the model the model and 
-        // it's 'persistentValue' (cache) will be updated to reflect the 'actual' database value (without fractional seconds). 
-        // 
-        // To avoid issues (e.g., during testing), we'll just make sure the hibernate cache reflects the 'actual' persisted value, 
+        // Unfortunately, 'lastModified' is mapped to SQL DATE type versus SQL TIMESTAMP, and thus does not retain fractional seconds.
+        // More unfortunately, the Hibernate cache does not reflect this, but will hold the 'lastModified' instance with fractional seconds
+        // as the 'persistentValue'. Usually this is ok, but if a 'refresh()' is subsequently performed on the model the model and
+        // it's 'persistentValue' (cache) will be updated to reflect the 'actual' database value (without fractional seconds).
+        //
+        // To avoid issues (e.g., during testing), we'll just make sure the hibernate cache reflects the 'actual' persisted value,
         // by removing the fractional seconds.
         Calendar cal = Calendar.getInstance()
         cal.setTime( new Date() )
-        cal.set( Calendar.MILLISECOND, 0 ) // truncate fractional seconds, so that we can compare dates to those retrieved from the database   
+        cal.set( Calendar.MILLISECOND, 0 ) // truncate fractional seconds, so that we can compare dates to those retrieved from the database
         Date lastModified = new Date( cal.getTime().getTime() )
 
         (setPropertyValue( event, "dataOrigin" ) { CH.config?.dataOrigin ?: "Banner" }
-            && setPropertyValue( event, "lastModifiedBy" ) { SCH.context?.authentication?.principal?.username }
+            && setPropertyValue( event, "lastModifiedBy" ) { getLastModifiedBy() }
             && setPropertyValue( event, "lastModified" ) { lastModified })
     }
 
