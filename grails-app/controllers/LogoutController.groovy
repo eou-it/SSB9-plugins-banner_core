ef9d26ec071238f85df1c6b6a22fa53e94f2d784
Jay Kadiam <jayachandra.kadiam@sungard.com>
2011-12-07 15:58:14 +0530

    HWF-973 - Fixed Signout link error

--- grails-app/controllers/LogoutController.groovy
+++ grails-app/controllers/LogoutController.groovy
@@ -23,7 +23,7 @@ class LogoutController {
      * Index action. Redirects to the Spring security logout uri.
      */
     def index = {
-        redirect uri: buildRedirectURI()
+        redirect uri: ControllerUtils.buildLogoutRedirectURI()
     }
 
 
