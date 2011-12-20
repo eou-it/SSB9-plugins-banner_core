e0daf29ae9cac4fd6f0615e1831bac34b5b938bf
Jonathan Miller <jonathan.miller@sungardhe.com>
2011-12-19 19:56:57 -0500

    HWF-1050 added support for sorting on multiple columns to the QueryBuilder

--- grails-app/utils/com/sungardhe/banner/query/QueryBuilder.groovy
+++ grails-app/utils/com/sungardhe/banner/query/QueryBuilder.groovy
@@ -12,7 +12,7 @@ package com.sungardhe.banner.query
 
 class QueryBuilder {
 
-    public static def buildQuery = { query, tableIdentifier,  criteria,pagingAndSortParams ->
+    public static def buildQuery = { query, tableIdentifier, criteria, pagingAndSortParams ->
 
         def returnQuery = query
 
@@ -24,14 +24,20 @@ class QueryBuilder {
             returnQuery += CriteriaOperatorFactory.operators."${it.operator}"?.dynamicQuery(tableIdentifier,it)
         }
 
-        if( pagingAndSortParams.sortColumn && (pagingAndSortParams.sortDirection!=null) ){
-             returnQuery += "order by  $tableIdentifier.$pagingAndSortParams.sortColumn  $pagingAndSortParams.sortDirection"
-        } else if(pagingAndSortParams.sortColumn){
-              // we are not using tableIdentifier since there is only one table identifier
-              // and no need to add table identifier
-              // and there is not provision for multiple table identifiers as of now.
-              String sort =  (pagingAndSortParams.sortColumn as String).replaceAll("@@table@@", "$tableIdentifier." )
-              returnQuery += "order by $sort "
+        if (pagingAndSortParams.sortCriteria && pagingAndSortParams.sortCriteria instanceof Collection) {
+            def sortParams = pagingAndSortParams.sortCriteria.collect { "$tableIdentifier.$it.sortColumn  $it.sortDirection" }
+
+            returnQuery += "order by " + sortParams.join(", ")
+        } else {
+            if( pagingAndSortParams.sortColumn && (pagingAndSortParams.sortDirection!=null) ){
+                 returnQuery += "order by  $tableIdentifier.$pagingAndSortParams.sortColumn  $pagingAndSortParams.sortDirection"
+            } else if(pagingAndSortParams.sortColumn){
+                  // we are not using tableIdentifier since there is only one table identifier
+                  // and no need to add table identifier
+                  // and there is not provision for multiple table identifiers as of now.
+                  String sort =  (pagingAndSortParams.sortColumn as String).replaceAll("@@table@@", "$tableIdentifier." )
+                  returnQuery += "order by $sort "
+            }
         }
 
         return returnQuery
