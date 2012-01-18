65fe746d81e0c9ca522e0d8b82010650bd5c37e0
Rajesh Kumar <rajesh.kumar@sungardhe.com>
2012-01-18 08:28:42 -0500

    Update banner code 1.0.40

--- BannerCoreGrailsPlugin.groovy
+++ BannerCoreGrailsPlugin.groovy
@@ -66,7 +66,7 @@ class BannerCoreGrailsPlugin {
 
     String groupId = "com.sungardhe"
 
-    String version = "1.0.39"
+    String version = "1.0.40"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.0 > *"
