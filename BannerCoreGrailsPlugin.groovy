b6a36c0b5a40dffc70d689a6a436a97fff72f62e
Navein Austin Fernandes <Navein.Fernandes@sungard.com>
2012-01-20 12:40:47 +0530

    HEM-696, HEM-576, HRU-3359 - Changes for generating menus and breadcrumbs for events.

--- BannerCoreGrailsPlugin.groovy
+++ BannerCoreGrailsPlugin.groovy
@@ -66,7 +66,7 @@ class BannerCoreGrailsPlugin {
 
     String groupId = "com.sungardhe"
 
-    String version = "1.0.40"
+    String version = "1.0.41"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.0 > *"
