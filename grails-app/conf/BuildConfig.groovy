01189aa172ca18cfbb22603f9810515a26859d5b
Anish Kumar <anish.kumar@sungardhe.com>
2012-04-11 17:22:52 -0400

    Updated BuildConfig and application properties to use in-place plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -15,6 +15,9 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir	= "target/test-reports"
 
+grails.plugin.location.'spring-security-cas'="../spring_security_cas.git"
+grails.plugin.location.'banner-codenarc'="../banner_codenarc.git"
+
 grails.project.dependency.resolution = {
     
     // inherit Grails' default dependencies
@@ -26,26 +29,13 @@ grails.project.dependency.resolution = {
     
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
 
-     plugins {
-        compile 'com.sungardhe:spring-security-core:1.0.1'
-        compile 'com.sungardhe:spring-security-cas:1.0.2' // Note: Also update version within 'application.properties'=
-    }
     
     distribution = {
          localRepository = ""
-         remoteRepository( id:"snapshots", url:"http://m038083.sungardhe.com:8081/nexus/content/repositories/snapshots" ) {
-              authentication  username:'admin', password:'admin123'
-         }
-         remoteRepository( id:"releases", url:"http://m038083.sungardhe.com:8081/nexus/content/repositories/releases" ) {
-             authentication  username:'admin', password:'admin123'
-         }
     }  
       
     
     repositories {
-        mavenRepo "http://m038083.sungardhe.com:8081/nexus/content/repositories/releases/"
-        mavenRepo "http://m038083.sungardhe.com:8081/nexus/content/repositories/snapshots/"
-        mavenRepo "http://m038083.sungardhe.com:8081/nexus/content/repositories/thirdparty/"
         
         grailsPlugins()
         grailsHome()
@@ -56,10 +46,6 @@ grails.project.dependency.resolution = {
         mavenRepo "http://repository.codehaus.org"
     }
 
-    plugins {
-	    provided 'com.sungardhe:banner-codenarc:0.1.3'// Note: Also update version within 'application.properties'
-	    provided 'com.sungardhe:code-coverage:1.2'    // Note: Also update version within 'application.properties'
-	}
     
     dependencies {
     }
