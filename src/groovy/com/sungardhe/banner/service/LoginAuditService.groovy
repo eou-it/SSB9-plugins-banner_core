d1452d9dcead0fa9edd2043cde25e0cb6b969963
Rajesh Kumar <rajesh.kumar@sungardhe.com>
2012-07-09 14:01:57 -0400

    Modified auditservice for HRU 3657

--- src/groovy/com/sungardhe/banner/service/LoginAuditService.groovy
+++ src/groovy/com/sungardhe/banner/service/LoginAuditService.groovy
@@ -37,7 +37,7 @@ public class LoginAuditService implements ApplicationListener<BannerAuthenticati
             conn = dataSource.unproxiedConnection
             sql = new Sql( conn )
             if (event.userName?.size() > 0 && event.module?.size() > 0 )  {
-                def truncateUserName = event.userName[0..(event.userName.size() > 29 ? 29 : event.userName.size())]
+                def truncateUserName = event.userName[0..(event.userName.size() > 29 ? 29 : event.userName.size() - 1)]
                 sql.call("begin g\$_security.g\$_check_logon_rules('BAN9',?); commit; end;",[truncateUserName])
             }
         } catch (Exception e) {
@@ -54,7 +54,7 @@ public class LoginAuditService implements ApplicationListener<BannerAuthenticati
             conn = dataSource.unproxiedConnection
             sql = new Sql( conn )
             if (event.userName?.size() > 0 && event.module?.size() > 0 )  {
-                def truncateUserName = event.userName[0..(event.userName.size() > 29 ? 29 : event.userName.size())]
+                def truncateUserName = event.userName[0..(event.userName.size() > 29 ? 29 : event.userName.size() - 1)]
                 sql.call("begin g\$_security.g\$_create_log_record(?,?,?,?); commit; end;",[truncateUserName,event.module,event.message, event.severity])
             }
         } catch (Exception e) {
