7d2beff0288f7aea118a6498186142f963a09340
Naveen Prapulla S <naveenprapulla.s@ellucian.com>
2013-08-02 13:00:44 +0530

    Changes related to Terms of usage and Survey page

---
+++ src/groovy/net/hedtech/banner/loginworkflow/UserAgreementFlow.groovy
@@ -0,0 +1,93 @@
+package net.hedtech.banner.loginworkflow
+
+import groovy.sql.GroovyRowResult
+import groovy.sql.Sql
+import net.hedtech.banner.security.BannerUser
+import org.springframework.security.core.context.SecurityContextHolder
+
+import java.sql.SQLException
+
+/** *****************************************************************************
+ © 2013 SunGard Higher Education.  All Rights Reserved.
+
+ CONFIDENTIAL BUSINESS INFORMATION
+
+ THIS PROGRAM IS PROPRIETARY INFORMATION OF SUNGARD HIGHER EDUCATION
+ AND IS NOT TO BE COPIED, REPRODUCED, LENT, OR DISPOSED OF,
+ NOR USED FOR ANY PURPOSE OTHER THAN THAT WHICH IT IS SPECIFICALLY PROVIDED
+ WITHOUT THE WRITTEN PERMISSION OF THE SAID COMPANY
+ ****************************************************************************** */
+
+class UserAgreementFlow implements PostLoginWorkflow {
+    def dataSource               // injected by Spring
+    def sessionFactory
+    public boolean showPage(request) {
+        def session = request.getSession();
+        String isDone = session.getAttribute("useraggrementdone")
+        String pidm = getPidm()
+        String displayStatus = getTermsOfUsageDisplayStatus()
+        boolean displayPage = false
+        if(displayStatus?.equals("Y"))
+        {
+            String usageIndicator = getUsageIndicator(pidm)
+            if(usageIndicator?.equals("N")){
+                displayPage = true
+            }
+
+        }else{
+            displayPage = false
+        }
+        return displayPage
+    }
+
+    public String getControllerUri() {
+        return "/ssb/userAgreement"
+    }
+
+    public static String getPidm() {
+        def user = SecurityContextHolder?.context?.authentication?.principal
+        if (user instanceof BannerUser) {
+            return user.pidm
+        }
+        return null
+    }
+    public String getTermsOfUsageDisplayStatus(){
+        def connection
+        Sql sql
+        try{
+            connection = sessionFactory.currentSession.connection()
+            sql = new Sql(connection)
+            GroovyRowResult row = sql.firstRow("""select TWGBWRUL_DISP_USAGE_IND from TWGBWRUL""")
+            return row.TWGBWRUL_DISP_USAGE_IND
+        }catch (SQLException ae) {
+            sql.close()
+            log.debug ae.stackTrace
+            throw ae
+        }
+        catch (Exception ae) {
+            log.debug ae.stackTrace
+            throw ae
+        }
+    }
+
+    public String getUsageIndicator(String pidm){
+        def connection
+        Sql sql
+        try{
+            connection = sessionFactory.currentSession.connection()
+            sql = new Sql(connection)
+            GroovyRowResult row = sql.firstRow("""select GOBTPAC_USAGE_ACCEPT_IND from GOBTPAC where GOBTPAC_PIDM = ${pidm}""")
+            return row.GOBTPAC_USAGE_ACCEPT_IND
+        }catch (SQLException ae) {
+            sql.close()
+            log.debug ae.stackTrace
+            throw ae
+        }
+        catch (Exception ae) {
+            log.debug ae.stackTrace
+            throw ae
+        }
+    }
+
+
+}
