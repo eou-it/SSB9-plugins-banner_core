0c144075a780ff3bdd9f55548c2145ecfbd2f39e
Suganthi <Suganthi.Manoharan@ellucian.com>
2013-08-27 16:31:15 +0530

    Added semicolon

--- web-app/js/views/userAgreement/policy.js
+++ web-app/js/views/userAgreement/policy.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     $("#policy-continue").click(function () {
-        var href = $(this).attr("data-endpoint")
-        window.location = href
+        var href = $(this).attr("data-endpoint");
+        window.location = href;
     });
     $("#policy-exit").click(function () {
-        var href = $(this).attr("data-endpoint")
-        window.location = href
+        var href = $(this).attr("data-endpoint");
+        window.location = href;
     });
 
 })
