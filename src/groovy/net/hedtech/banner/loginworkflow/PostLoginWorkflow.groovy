7537bc95bcab31d9ddbadee9ed2a8cbde1b7f83e
Suganthi <Suganthi.Manoharan@ellucian.com>
2013-09-26 11:12:27 +0530

    (HRU-4698) BannerSelfServicePostLoginFlowFilter code review comments

--- src/groovy/net/hedtech/banner/loginworkflow/PostLoginWorkflow.groovy
+++ src/groovy/net/hedtech/banner/loginworkflow/PostLoginWorkflow.groovy
@@ -10,24 +10,24 @@ package net.hedtech.banner.loginworkflow
  WITHOUT THE WRITTEN PERMISSION OF THE SAID COMPANY
  ****************************************************************************** */
 abstract class PostLoginWorkflow {
-    static Map flowEntries = new TreeMap();
-
+    private static Map<String,PostLoginWorkflow> flowEntries = new TreeMap();
     public static final String URI_ACCESSED = "SS_POST_lOGIN_WORKFLOW_URI_ACCESSED";
-    public static final String ALL_DONE = "SS_POST_lOGIN_WORKFLOW_ALL_DONE";
+    public static final String FLOW_COMPLETE = "SS_POST_lOGIN_WORKFLOW_COMPLETE";
 
-    public abstract boolean showPage(request);
+    public abstract boolean isShowPage(request);
     public abstract String getControllerUri();
     public abstract String getControllerName();
 
-    public setRegisterFlowClass(Map entryMap) {
+    public setRegisterFlowClass(Map<Integer, String> entryMap) {
         Set <Object>keySet = entryMap.keySet();
         for(Object key : keySet)
         {
-            flowEntries.put(key, entryMap.get(key));
+            flowEntries.put(key, this);
         }
     }
 
-    public static List getListOfFlows() {
+    public static List<PostLoginWorkflow> getListOfFlows() {
         return new ArrayList(flowEntries.values());
     }
+
 }
