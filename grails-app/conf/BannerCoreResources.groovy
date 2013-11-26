43bc24fa1815872300fe15cb1dacb206fe6d5255
shrridevis <shrridevi.sreedharan@ellucian.com>
2013-11-26 17:58:52 +0530

    HRU-4840 Incorporating code review comments on page to page navigation

--- grails-app/conf/BannerCoreResources.groovy
+++
@@ -1,8 +0,0 @@
-/* Copyright 2009-2013 Ellucian Company L.P. and its affiliates. */
-
-modules = {
-   'jqueryForBannerCore' {
-       defaultBundle environment == "development" ? false : "jqueryForBannerCore"
-       resource url:[plugin: 'banner-core', file: 'js/jquery/jquery-1.4.2.js'], disposition: 'head'
-   }
-}
