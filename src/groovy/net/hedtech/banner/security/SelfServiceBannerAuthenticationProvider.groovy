9464419a43f133d6a66cda21aa5199138d5848fe
SphoortiA <sphoorti.acharya@ellucian.com>
2014-07-31 12:32:13 +0530

    Adding back the code reverted on 5/18 - admin auth for self service

--- src/groovy/net/hedtech/banner/security/SelfServiceBannerAuthenticationProvider.groovy
+++ src/groovy/net/hedtech/banner/security/SelfServiceBannerAuthenticationProvider.groovy
@@ -299,7 +299,9 @@ public class SelfServiceBannerAuthenticationProvider implements AuthenticationPr
 
        // def selfServiceRolePassword
         if (authentictionResults.oracleUserName) {
-                authorities << BannerGrantedAuthority.create( "SELFSERVICE", "BAN_DEFAULT_M", null )
+            authorities << BannerGrantedAuthority.create( "SELFSERVICE", "BAN_DEFAULT_M", null )
+            Collection<GrantedAuthority> adminAuthorities = BannerAuthenticationProvider.determineAuthorities( authentictionResults, db )
+            authorities.addAll( adminAuthorities )
         }
        log.trace "SelfServiceAuthenticationProvider.determineAuthorities will return $authorities"
        authorities
