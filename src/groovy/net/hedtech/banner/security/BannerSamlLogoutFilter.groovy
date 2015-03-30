384ccc72ae6dbe3ae5637136fc606593f85199b3
SphoortiA <sphoorti.acharya@ellucian.com>
2015-03-30 13:32:04 +0530

    GAF-795 fix for logout stackoverflow

--- src/groovy/net/hedtech/banner/security/BannerSamlLogoutFilter.groovy
+++ src/groovy/net/hedtech/banner/security/BannerSamlLogoutFilter.groovy
@@ -7,35 +7,66 @@ import org.apache.log4j.Logger
 import org.opensaml.common.SAMLException
 import org.opensaml.saml2.metadata.provider.MetadataProviderException
 import org.opensaml.ws.message.encoder.MessageEncodingException
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.saml.SAMLAuthenticationToken
-import org.springframework.security.saml.SAMLConstants
 import org.springframework.security.saml.SAMLCredential
-import org.springframework.security.saml.SAMLLogoutFilter
+import org.springframework.security.saml.context.SAMLContextProvider
 import org.springframework.security.saml.context.SAMLMessageContext
+import org.springframework.security.saml.util.SAMLUtil
+import org.springframework.security.saml.websso.SingleLogoutProfile
+import org.springframework.security.web.FilterInvocation
+import org.springframework.security.web.authentication.logout.LogoutFilter
 import org.springframework.security.web.authentication.logout.LogoutHandler
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler
 import org.springframework.util.Assert
 
 import javax.servlet.FilterChain
 import javax.servlet.ServletException
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 /**
  * An Filter added to handle the SAMl logout with token of type BannerAuthenticationToken.
  */
-class BannerSamlLogoutFilter extends SAMLLogoutFilter{
+class BannerSamlLogoutFilter extends LogoutFilter {
 
     private static final Logger log = Logger.getLogger( "net.hedtech.banner.security.BannerSamlLogoutFilter" )
+    protected SAMLContextProvider contextProvider
+    protected SingleLogoutProfile profile
+    /**
+     * Name of parameter of HttpRequest indicating whether this call should perform only local logout.
+     * In case the value is true no global logout will be invoked.
+     */
+    protected static final String LOGOUT_PARAMETER = "local"
+    /**
+     * Handlers to be invoked during logout.
+     */
+    protected LogoutHandler[] globalHandlers
+
+    /**
+     * URL this filter processes
+     */
+    public static final String FILTER_URL = "/saml/logout";
 
     BannerSamlLogoutFilter(String successUrl, LogoutHandler[] localHandler, LogoutHandler[] globalHandlers) {
-        super(successUrl, localHandler, globalHandlers)
+        super(successUrl, localHandler)
+        this.globalHandlers = globalHandlers
+        this.setFilterProcessesUrl(FILTER_URL)
     }
 
     BannerSamlLogoutFilter(LogoutSuccessHandler logoutSuccessHandler, LogoutHandler[] localHandler, LogoutHandler[] globalHandlers) {
-        super(logoutSuccessHandler, localHandler, globalHandlers)
+        super(logoutSuccessHandler, localHandler)
+        this.globalHandlers = globalHandlers
+        this.setFilterProcessesUrl(FILTER_URL)
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        FilterInvocation fi = new FilterInvocation(request, response, chain)
+        processLogout(fi.getRequest(), fi.getResponse(), chain)
     }
 
     public void processLogout(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -43,14 +74,13 @@ class BannerSamlLogoutFilter extends SAMLLogoutFilter{
         if (requiresLogout(request, response)) {
 
             try {
-               BannerAuthenticationToken auth=SecurityContextHolder.getContext().getAuthentication();
+                BannerAuthenticationToken auth=SecurityContextHolder.getContext().getAuthentication();
                 if (auth != null && isGlobalLogout(request,auth)) {
                     if(!(auth instanceof BannerAuthenticationToken)) {
                         log.fatal("Authentication object doesn't contain SAML credential, cannot perform global logout")
                         throw new ServletException("Authentication object doesn't contain SAML credential, cannot perform global logout")
                     }
-                    Assert.isInstanceOf(BannerAuthenticationToken.class,auth,"Authentication object doesn't contain SAML credential, cannot perform global logout")
-                    for (        LogoutHandler handler : globalHandlers) {
+                    for (LogoutHandler handler : globalHandlers) {
                         handler.logout(request,response,auth)
                     }
                     SAMLMessageContext context = contextProvider.getLocalEntity(request,response,(SAMLCredential)auth.getSAMLCredential())
@@ -58,7 +88,7 @@ class BannerSamlLogoutFilter extends SAMLLogoutFilter{
                     log.debug("Logout Request initiated with ontext : " + context)
                 }
                 else {
-                    super.doFilter(request,response,chain);
+                    super.doFilter(request,response,chain)
                 }
             } catch (SAMLException e1) {
                 log.fatal("Error initializing global logout " + e1)
@@ -71,11 +101,21 @@ class BannerSamlLogoutFilter extends SAMLLogoutFilter{
                 throw new ServletException("Error encoding outgoing message", e1)
             }
         } else {
-            chain.doFilter(request, response);
+            chain.doFilter(request, response)
         }
 
     }
 
+    /**
+     * The filter will be used in case the URL of the request contains the DEFAULT_FILTER_URL.
+     *
+     * @param request request used to determine whether to enable this filter
+     * @return true if this filter should be used
+     */
+    @Override
+    protected boolean requiresLogout(HttpServletRequest request, HttpServletResponse response) {
+        return SAMLUtil.processFilter(getFilterProcessesUrl(), request)
+    }
 
     /**
      * Performs global logout in case current user logged in using SAML and user hasn't selected local logout only
@@ -85,11 +125,31 @@ class BannerSamlLogoutFilter extends SAMLLogoutFilter{
      * @return true if single logout with IDP is required
      */
     protected boolean isGlobalLogout(HttpServletRequest request, Authentication auth) {
-        String login = request.getParameter(LOGOUT_PARAMETER);
-        return (login == null || !"true".equals(login.toLowerCase().trim())) && (auth instanceof BannerAuthenticationToken);
+        String login = request.getParameter(LOGOUT_PARAMETER)
+        return (login == null || !"true".equals(login.toLowerCase().trim())) && (auth instanceof BannerAuthenticationToken)
     }
 
+    /**
+     * Profile for consumption of processed messages, cannot be null, must be set.
+     *
+     * @param profile profile
+     */
+    @Autowired
+    public void setProfile(SingleLogoutProfile profile) {
+        Assert.notNull(profile, "SingleLogoutProfile can't be null")
+        this.profile = profile
+    }
 
+    /**
+     * Sets entity responsible for populating local entity context data. Cannot be null, must be set.
+     *
+     * @param contextProvider provider implementation
+     */
+    @Autowired
+    public void setContextProvider(SAMLContextProvider contextProvider) {
+        Assert.notNull(contextProvider, "Context provider can't be null")
+        this.contextProvider = contextProvider
+    }
 
 
 }
