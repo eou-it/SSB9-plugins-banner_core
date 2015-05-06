9c5348da9f8409e1c44a1f14ffecd74614d8b742
mohit jain <mohit.jain@ellucian.com>
2015-05-06 11:54:00 +0530

    GAF-837 code changes as suggested by sphoorti in BannerSamlSavedRequestAwareAuthenticationSuccessHandler

--- src/groovy/net/hedtech/banner/security/BannerSamlSavedRequestAwareAuthenticationSuccessHandler.groovy
+++ src/groovy/net/hedtech/banner/security/BannerSamlSavedRequestAwareAuthenticationSuccessHandler.groovy
@@ -5,55 +5,28 @@ package net.hedtech.banner.security
 
 import org.apache.log4j.Logger
 import org.springframework.security.core.Authentication
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache
-import org.springframework.security.web.savedrequest.RequestCache
-import org.springframework.security.web.savedrequest.SavedRequest
-import org.springframework.util.StringUtils
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler
 
 import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-class BannerSamlSavedRequestAwareAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+class BannerSamlSavedRequestAwareAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private static final Logger log = Logger.getLogger( BannerSamlSavedRequestAwareAuthenticationSuccessHandler.class )
-
-    private RequestCache requestCache = new HttpSessionRequestCache();
     BannerSamlSessionRegistryImpl sessionRegistry;
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws ServletException, IOException {
-        SavedRequest savedRequest = requestCache.getRequest(request, response);
 
         BannerAuthenticationToken token = (BannerAuthenticationToken) authentication;
         String sessionIndex= token.getsessionIndex();
         log.debug  "BannerSamlSavedRequestAwareAuthenticationSuccessHandler.onAuthenticationSuccess adding new session in sessionregistry $sessionIndex"
         sessionRegistry.registerNewSession(request.getSession(),sessionIndex);
-
-        if (savedRequest == null) {
-            super.onAuthenticationSuccess(request, response, authentication);
-
-            return;
-        }
-
-        if (isAlwaysUseDefaultTargetUrl() || StringUtils.hasText(request.getParameter(getTargetUrlParameter()))) {
-            requestCache.removeRequest(request, response);
-            super.onAuthenticationSuccess(request, response, authentication);
-
-            return;
-        }
-
+        super.onAuthenticationSuccess(request, response, authentication);
         clearAuthenticationAttributes(request);
 
-        // Use the DefaultSavedRequest URL
-        String targetUrl = savedRequest.getRedirectUrl();
-        logger.debug("Redirecting to DefaultSavedRequest Url: " + targetUrl);
-
-        getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 
-    public void setRequestCache(RequestCache requestCache) {
-        this.requestCache = requestCache;
-    }
+
 }
