98a0308676c3f86ad3006b0a732b55d95f03b376
SphoortiA <sphoorti.acharya@ellucian.com>
2015-06-01 17:15:49 +0530

    Fix the issue with wrong class implementation due to which the saved request was not handled

--- src/groovy/net/hedtech/banner/security/BannerSamlSavedRequestAwareAuthenticationSuccessHandler.groovy
+++ src/groovy/net/hedtech/banner/security/BannerSamlSavedRequestAwareAuthenticationSuccessHandler.groovy
@@ -5,13 +5,13 @@ package net.hedtech.banner.security
 
 import org.apache.log4j.Logger
 import org.springframework.security.core.Authentication
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler
 
 import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-class BannerSamlSavedRequestAwareAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+class BannerSamlSavedRequestAwareAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
     private static final Logger log = Logger.getLogger( BannerSamlSavedRequestAwareAuthenticationSuccessHandler.class )
     BannerSamlSessionRegistryImpl sessionRegistry;
