8c56bcbd4d991ee24cf4aed0d98d5a2934287a0c
Barun Kumar Patel <barun.patel@ellucian.com>
2016-10-27 16:59:40 +0530

    Integration Test changes.

--- test/integration/net/hedtech/banner/general/utility/PreferredNameServiceIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/general/utility/PreferredNameServiceIntegrationTests.groovy
@@ -4,21 +4,14 @@
 
 package net.hedtech.banner.general.utility
 
-import grails.spring.BeanBuilder
-import grails.util.Environment
 import grails.util.Holders
-import grails.util.Holders  as CH
 import groovy.sql.Sql
 import net.hedtech.banner.exceptions.ApplicationException
 import net.hedtech.banner.i18n.MessageHelper
 import net.hedtech.banner.testing.BaseIntegrationTestCase
-import org.apache.commons.dbcp.BasicDataSource
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import org.springframework.context.ApplicationContext
-
-import javax.sql.DataSource
 
 class PreferredNameServiceIntegrationTests extends BaseIntegrationTestCase   {
     def params
@@ -30,36 +23,29 @@ class PreferredNameServiceIntegrationTests extends BaseIntegrationTestCase   {
     def preferredNameService
     public final String LF30= "LF30"
     public final String L30= "L30"
-    public final String L60= "L60"
     public final String FL30= "FL30"
     public final String FL= "FL"
     public final String FMIL= "FMIL"
 
     public final String LFMI= "LFMI"
     public final String DEFAULT= "DEFAULT"
-    public final String LEGAL= "LEGAL"
-    DataSource underlyingDataSource
-    DataSource underlyingSsbDataSource
 
 
     @Before
     public void setUp() {
-        Holders?.config.ssbEnabled = true
+        formContext = ['GUAGMNU']
+        super.setUp()
         conn = dataSource.getSsbConnection()
         sqlObj = new Sql( conn )
-        formContext = ['GUAGMNU']
-        def config = CH.getConfig()
-        config.ssbEnabled = true
         pidm =  getPidmBySpridenId('SJGRIM')
         usage = DEFAULT
-        super.setUp()
     }
 
     @After
     public void tearDown() {
+        super.tearDown()
         sqlObj.close()
         conn.close()
-        super.tearDown()
     }
 
     @Test
@@ -222,32 +208,6 @@ class PreferredNameServiceIntegrationTests extends BaseIntegrationTestCase   {
         assertEquals DEFAULT, defaultName
     }
 
-    private ApplicationContext createUnderlyingSsbDataSourceBean() {
-        def bb = new BeanBuilder()
-        bb.beans {
-            underlyingSsbDataSource(BasicDataSource) {
-                maxActive = 5
-                maxIdle = 2
-                defaultAutoCommit = "false"
-                driverClassName = "${CH.config.bannerSsbDataSource.driver}"
-                url = "${CH.config.bannerSsbDataSource.url}"
-                password = "${CH.config.bannerSsbDataSource.password}"
-                username = "${CH.config.bannerSsbDataSource.username}"
-            }
-
-            underlyingDataSource(BasicDataSource) {
-                maxActive = 5
-                maxIdle = 2
-                defaultAutoCommit = "false"
-                driverClassName = "${CH.config.bannerDataSource.driver}"
-                url = "${CH.config.bannerDataSource.url}"
-                password = "${CH.config.bannerDataSource.password}"
-                username = "${CH.config.bannerDataSource.username}"
-            }
-        }
-        ApplicationContext testSpringContext = bb.createApplicationContext()
-        return testSpringContext
-    }
 
     private def getPidmBySpridenId(def spridenId) {
         def query = "SELECT SPRIDEN_PIDM pidm FROM SPRIDEN WHERE SPRIDEN_ID=$spridenId"
