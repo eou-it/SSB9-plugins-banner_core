5d7d001ebbfcd36329ba131c07456488ce16c161
Eshaan_kumar <eshaan.kumar@ellucian.com>
2016-11-07 16:00:22 +0530

    Code review comments implemented

--- test/integration/net/hedtech/banner/utility/DateUtilityIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/utility/DateUtilityIntegrationTests.groovy
@@ -1,22 +1,21 @@
+/*******************************************************************************
+ Copyright 2009-2016 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
 package net.hedtech.banner.utility
 
-import grails.util.Holders
 import net.hedtech.banner.testing.BaseIntegrationTestCase
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import net.hedtech.banner.utility.DateUtility
 
 /**
- * Created by eshaank on 11/4/2016.
+ * Integration test file for DateUtility class.
  */
 class DateUtilityIntegrationTests extends BaseIntegrationTestCase {
-    def dateUtility
 
     @Before
     public void setUp() {
         formContext = ['GUAGMNU']
-        dateUtility = new DateUtility()
         super.setUp()
     }
 
@@ -28,7 +27,7 @@ class DateUtilityIntegrationTests extends BaseIntegrationTestCase {
     @Test
     void testGetTodayDate(){
         def outcome
-        outcome=dateUtility.getTodayDate()
+        outcome=DateUtility.getTodayDate()
         assertNotNull(outcome)
     }
 }
