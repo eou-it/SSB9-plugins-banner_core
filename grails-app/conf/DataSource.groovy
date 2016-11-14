11d7951234062527f3f642e4020a3f69f91372e5
snehils <snehil.singh@ellucian.com>
2016-11-14 13:09:00 +0530

    Revert "Merge branch 'release/platform-9.20' of https://git.ellucian.com/scm/banxeplug/banner_core into release/platform-9.20"
    This reverts commit 1b13c3e342bdaa2eae62037764da4aba6fa6b3c1, reversing
    changes made to 186227a878d866b01859928f8fa64d019f764ac2.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,57 +1,48 @@
+/*******************************************************************************
+ Copyright 2009-2015 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
+
+// Support Hibernate annotations
+import org.codehaus.groovy.grails.orm.hibernate.cfg.GrailsAnnotationConfiguration
+
+
+// Note: Most of the dataSource configuration resides in resources.groovy and in the
+// installation-specific configuration file (see Config.groovy for the include).
+//
 dataSource {
-    pooled = true
-    jmxExport = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    configClass = GrailsAnnotationConfiguration.class
+    dialect = "org.hibernate.dialect.Oracle10gDialect"
+    //loggingSql = false
 }
+
+
 hibernate {
     cache.use_second_level_cache = true
-    cache.use_query_cache = false
-//    cache.region.factory_class = 'org.hibernate.cache.SingletonEhCacheRegionFactory' // Hibernate 3
-    cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory' // Hibernate 4
-    singleSession = true // configure OSIV singleSession mode
-    flush.mode = 'manual' // OSIV session flush mode outside of transactional context
+    cache.use_query_cache = true
+    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+    hbm2ddl.auto = null
+    //show_sql = true
+//   	naming_strategy = "org.hibernate.cfg.ImprovedNamingStrategy"
+    dialect = "org.hibernate.dialect.Oracle10gDialect"
+    config.location = [
+            "classpath:hibernate-banner-core.cfg.xml",
+            "classpath:hibernate-banner-core.testing.cfg.xml"
+    ]
 }
+
 
 // environment specific settings
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
     test {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-            properties {
-               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
-               jmxEnabled = true
-               initialSize = 5
-               maxActive = 50
-               minIdle = 5
-               maxIdle = 25
-               maxWait = 10000
-               maxAge = 10 * 60000
-               timeBetweenEvictionRunsMillis = 5000
-               minEvictableIdleTimeMillis = 60000
-               validationQuery = "SELECT 1"
-               validationQueryTimeout = 3
-               validationInterval = 15000
-               testOnBorrow = true
-               testWhileIdle = true
-               testOnReturn = false
-               jdbcInterceptors = "ConnectionState"
-               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
-            }
         }
     }
 }
