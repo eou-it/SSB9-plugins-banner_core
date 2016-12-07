2252f16171e36e7fc9f75e6e16c2d41135ea7526
Harshal Bramhankar <Harshal.Bramhankar@ellucian.com>
2016-12-07 11:56:22 +0530

    HRU-7007 Code Coverage for Error Controller.

---
+++ test/integration/ErrorControllerIntegrationTests.groovy
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ Copyright 2016 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
+
+import net.hedtech.banner.exceptions.MepCodeNotFoundException
+import net.hedtech.banner.testing.BaseIntegrationTestCase
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class ErrorControllerIntegrationTests extends BaseIntegrationTestCase {
+
+    def controller
+
+    @Before
+    public void setUp() {
+        formContext = ['GUAGMNU']
+        super.setUp()
+        controller = new ErrorController()
+    }
+
+    @After
+    public void tearDown() {
+        super.tearDown()
+    }
+
+    @Test
+    void testAccessForbidden() {
+        controller.accessForbidden()
+        assertEquals(200, controller.response.status)
+    }
+
+    @Test
+    void testPageNotFoundError() {
+        controller.pageNotFoundError()
+        assertEquals(200, controller.response.status)
+    }
+
+    @Test
+    void testInternalServerErrorWithoutException() {
+        controller.internalServerError()
+        assertEquals(200, controller.response.status)
+    }
+
+    @Test
+    void testInternalServerError() {
+        Exception e = new Exception()
+        Throwable cause = new MepCodeNotFoundException()
+        e.initCause(cause)
+        controller.request.exception = e
+        controller.internalServerError()
+        assertEquals(200, controller.response.status)
+    }
+    
+}
