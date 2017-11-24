80ff29c80f1768742d190fd90c1827c61e729077
Harshal Bramhankar <harshal.bramhankar@ellucian.com>
2017-11-24 15:43:45 +0530

    Updating maven repo for Httpbuilder HRU-7435

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -37,12 +37,12 @@ grails.project.dependency.resolution = {
     repositories {
         if (System.properties['PROXY_SERVER_NAME']) {
             mavenRepo "${System.properties['PROXY_SERVER_NAME']}"
-            mavenRepo "http://repo.grails.org/grails/core"
         }
         grailsCentral()
         mavenCentral()
         mavenRepo "https://code.lds.org/nexus/content/groups/main-repo"
         mavenRepo "http://repository.jboss.org/maven2/"
+	mavenRepo "http://repo.grails.org/grails/core"
     }
 
     dependencies {
