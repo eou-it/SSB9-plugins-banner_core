149893eba30fdba533a8c274d8389b0d358178ed
Harshal Bramhankar <harshal.bramhankar@ellucian.com>
2017-12-12 15:47:19 +0530

    HRU-7566 changes for unused variable , imports and adding curly braces for else condition.

--- src/groovy/net/hedtech/banner/security/BannerAuthenticationFailureHandler.groovy
+++ src/groovy/net/hedtech/banner/security/BannerAuthenticationFailureHandler.groovy
@@ -4,9 +4,9 @@
 package net.hedtech.banner.security
 
 import grails.util.Holders
-import org.apache.log4j.Logger
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler
+
 import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -17,7 +17,6 @@ import javax.servlet.http.HttpServletResponse
 
 class BannerAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler{
 
-    private static final Logger log = Logger.getLogger( "net.hedtech.banner.security.BannerAuthenticationFailureHandler" )
 
     void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
 
