057af575904d60d34be8a5a770edded613edf954
Avinash Hinduja <avinash.hinduja@ellucian.com>
2018-05-30 09:39:37 +0530

    changing version and copyright

--- BannerCoreGrailsPlugin.groovy
+++ BannerCoreGrailsPlugin.groovy
@@ -43,7 +43,7 @@ import java.util.concurrent.Executors
  * */
 class BannerCoreGrailsPlugin {
 
-    String version = "9.28"
+    String version = "9.28.1"
     private static final Logger staticLogger = Logger.getLogger(BannerCoreGrailsPlugin.class)
 
     // the version or versions of Grails the plugin is designed for
